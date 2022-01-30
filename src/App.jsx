5e64ec5c2d9824a30d97d46331f9566ae2bcf2a7
Gabriel Lacerda <devlacerda1109@gmail.com>
2022-01-30 01:30:15 -0300

    Integração dos dados do data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import { ThemeProvider, responsiveFontSizes, Toolbar, Box } from '@material-ui/core';
-import logo from './assets/image/logo.png'
-import AppBody from './AppBody'
-import customTheme from './theme/customTheme'
+import {
+    ThemeProvider,
+    responsiveFontSizes,
+    Toolbar,
+    Box,
+    Container,
+} from "@material-ui/core";
+import logo from "./assets/image/logo.png";
+import AppBody from "./AppBody";
+import customTheme from "./theme/customTheme";
 
-let theme = customTheme
-theme = responsiveFontSizes(theme)
+let theme = customTheme;
+theme = responsiveFontSizes(theme);
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ height: '100%' }}>
-        <Toolbar sx={{boxShadow: '2px 0 10px 0 #ccc', backgroundColor: 'white'}}>
-          <img alt="logo" src={logo} />
-        </Toolbar>
-        <AppBody />
-      </Box>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Box sx={{ height: "100%" }}>
+                <Toolbar
+                    sx={{
+                        boxShadow: "2px 0 10px 0 #ccc",
+                        backgroundColor: "white",
+                    }}
+                >
+                    <Container maxWidth="xl">
+                        <img alt="logo" src={logo} />
+                    </Container>
+                </Toolbar>
+                <AppBody />
+            </Box>
+        </ThemeProvider>
+    );
 }
 
 export default App;
--- src/AppBody.jsx
+++ src/AppBody.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react'
-import { Box, Grid, Container, Typography, Button, FormControl, InputLabel, Select, MenuItem, Card, CardContent, IconButton, Menu } from '@material-ui/core'
-import { Add, MoreHoriz } from '@mui/icons-material';
-import Badge from './components/Badge';
+import React, { useState } from "react";
+import {
+    Box,
+    Grid,
+    Container,
+    Typography,
+    Button,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Card,
+    CardContent,
+    IconButton,
+    Menu,
+    Link,
+} from "@material-ui/core";
+import { Add, MoreHoriz } from "@mui/icons-material";
+import Badge from "./components/Badge";
+import { data } from "./assets/data/data";
 
 export default function AppBody() {
+    // DELETE MENU ------------------------------------------------------------------------------------------
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
     const handleClick = (event) => {
@@ -13,112 +30,229 @@ export default function AppBody() {
         setAnchorEl(null);
     };
 
+    // DATA HANDLING FUNCTIONS ------------------------------------------------------------------------------
+    function badgeType(type) {
+        let color;
+        switch (type) {
+            case "event":
+                color = "#EE8686";
+                break;
+            case "release":
+                color = "#3489B1";
+                break;
+            case "publication":
+                color = "#707070";
+                break;
+        }
+
+        return (
+            <Box sx={{ marginRight: "10px" }}>
+                <Badge text={type.toUpperCase()} color={color} />
+            </Box>
+        );
+    }
+
+    function confirmations(arr) {
+        let confirmed = arr.filter((e) => {
+            if (e.confirmed_presence == true) {
+                return e;
+            }
+        });
+
+        let str = `${confirmed.length} ${
+            confirmed.length == 1 ? "CONFIRMAÇÃO" : "CONFIRMAÇÕES"
+        } DE ${arr.length}`;
+
+        return str;
+    }
+
     return (
         <Box>
-            <Container maxWidth="xl" sx={{padding: '24px'}}>
+            <Container maxWidth="xl" sx={{ padding: "24px" }}>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} md={8} lg={9}> {/* ESQUERDA */}
+                    <Grid item xs={12} md={8} lg={9}>
+                        {" "}
+                        {/* ESQUERDA */}
                         <Box
                             sx={{
-                                display: 'flex',
-                                justifyContent: 'space-between',
-                                alignItems: 'center',
-                                flexWrap: 'wrap',
-                                gap: '20px'
+                                display: "flex",
+                                justifyContent: "space-between",
+                                alignItems: "center",
+                                flexWrap: "wrap",
+                                gap: "20px",
                             }}
                         >
-                            <Typography variant="h2" color="textPrimary">Endomarketing</Typography>
-                            <Box sx={{ display: 'flex', gap: '15px' }}>
-                                <FormControl size="small" sx={{ minWidth: '120px' }}>
+                            <Typography variant="h2" color="textPrimary">
+                                Endomarketing
+                            </Typography>
+                            <Box sx={{ display: "flex", gap: "15px" }}>
+                                <FormControl
+                                    size="small"
+                                    sx={{ minWidth: "120px" }}
+                                >
                                     <InputLabel>Tipo</InputLabel>
                                     {/* https://v5-0-6.mui.com/pt/components/selects/ - LINK PARA SELEÇÃO */}
                                     <Select
                                         label="Tipo"
-                                        sx={{backgroundColor: 'white'}}
+                                        sx={{ backgroundColor: "white" }}
                                     >
                                         <MenuItem value="">
                                             <em>Nenhum</em>
                                         </MenuItem>
-                                        <MenuItem value="com">Comunicado</MenuItem>
+                                        <MenuItem value="com">
+                                            Comunicado
+                                        </MenuItem>
                                         <MenuItem value="eve">Evento</MenuItem>
-                                        <MenuItem value="pub">Publicação</MenuItem>
+                                        <MenuItem value="pub">
+                                            Publicação
+                                        </MenuItem>
                                     </Select>
                                 </FormControl>
-                            <Button variant="contained" endIcon={<Add />}>Criar</Button>
+                                <Button variant="contained" endIcon={<Add />}>
+                                    Criar
+                                </Button>
                             </Box>
                         </Box>
                         {/* MAP */}
-                        <Box mt={2} sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                            <Card>
-                                <CardContent
-                                    sx={{
-                                        display: 'flex',
-                                        justifyContent: 'space-between',
-                                        alignItems: 'center',
-                                        padding: '16px !important'
-                                    }}
-                                >
-                                    <Box sx={{display: 'flex', gap: '16px'}}>
-                                        <Box
+                        <Box
+                            mt={2}
+                            sx={{
+                                display: "flex",
+                                flexDirection: "column",
+                                gap: "10px",
+                            }}
+                        >
+                            {data.map((el, i) => {
+                                return (
+                                    <Card key={i}>
+                                        <CardContent
                                             sx={{
-                                                backgroundColor: '#ccc',
-                                                width: '100px',
-                                                height: '100px'
+                                                display: "flex",
+                                                justifyContent: "space-between",
+                                                alignItems: "center",
+                                                padding: "16px !important",
                                             }}
-                                        ></Box>
-                                        <Box>
-                                            <Typography variant="h4">Título do card</Typography>
-                                            <Box>
-                                                <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
-                                                    <Badge text="COMUNICADO" color="#1976d2" />
-                                                    <Typography>13 DE OUT</Typography>
+                                        >
+                                            <Box
+                                                sx={{
+                                                    display: "flex",
+                                                    gap: "16px",
+                                                }}
+                                            >
+                                                <Box
+                                                    sx={{
+                                                        backgroundColor: "#ccc",
+                                                        width: "100px",
+                                                        height: "100px",
+                                                        backgroundImage: `url(${el.file.url})`,
+                                                        backgroundSize: "cover",
+                                                    }}
+                                                ></Box>
+                                                <Box>
+                                                    <Typography variant="h4">
+                                                        {el.title}
+                                                    </Typography>
+                                                    <Box>
+                                                        <Box
+                                                            sx={{
+                                                                display: "flex",
+                                                                alignItems: "center"
+                                                            }}
+                                                        >
+                                                            {badgeType(el.type)}
+                                                            {el.info.place ? (
+                                                                <Typography>{el.info.place} | </Typography>
+                                                            ) : null}
+                                                            <Typography>
+                                                                {el.info.date}
+                                                            </Typography>
+                                                            {el.type ===
+                                                            "event" ? (
+                                                                <Typography>
+                                                                    {" "}
+                                                                    |{" "}
+                                                                    <Link sx={{cursor: 'pointer'}}>
+                                                                        {confirmations(
+                                                                            el.invited_people
+                                                                        )}
+                                                                    </Link>
+                                                                </Typography>
+                                                            ) : null}
+                                                        </Box>
+                                                        <Typography mt={1}>
+                                                            {el.description}
+                                                        </Typography>
+                                                    </Box>
                                                 </Box>
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt...</p>
                                             </Box>
-                                        </Box>
-                                    </Box>
-                                    {/* https://v5-0-6.mui.com/pt/components/menus/ - LINK PARA MENU */}
-                                    <IconButton
-                                        sx={{ backgroundColor: '#dbdbdb' }}
-                                        id="basic-button"
-                                        aria-controls="basic-menu"
-                                        aria-haspopup="true"
-                                        aria-expanded={open ? 'true' : undefined}
-                                        onClick={handleClick}
-                                    >
-                                        <MoreHoriz sx={{fill: '#707070'}} />
-                                    </IconButton>
-                                    <Menu
-                                        id="basic-menu"
-                                        anchorEl={anchorEl}
-                                        open={open}
-                                        onClose={handleClose}
-                                        MenuListProps={{
-                                        'aria-labelledby': 'basic-button',
-                                        }}
-                                    >
-                                        <MenuItem onClick={handleClose}>Excluir</MenuItem>
-                                    </Menu>
-                                </CardContent>
-                            </Card>
+                                            {/* https://v5-0-6.mui.com/pt/components/menus/ - LINK PARA MENU */}
+                                            <IconButton
+                                                sx={{
+                                                    backgroundColor: "#dbdbdb",
+                                                }}
+                                                id="basic-button"
+                                                aria-controls="basic-menu"
+                                                aria-haspopup="true"
+                                                aria-expanded={
+                                                    open ? "true" : undefined
+                                                }
+                                                onClick={handleClick}
+                                            >
+                                                <MoreHoriz
+                                                    sx={{ fill: "#707070" }}
+                                                />
+                                            </IconButton>
+                                            <Menu
+                                                id="basic-menu"
+                                                anchorEl={anchorEl}
+                                                open={open}
+                                                onClose={handleClose}
+                                                MenuListProps={{
+                                                    "aria-labelledby":
+                                                        "basic-button",
+                                                }}
+                                            >
+                                                <MenuItem onClick={handleClose}>
+                                                    Excluir
+                                                </MenuItem>
+                                            </Menu>
+                                        </CardContent>
+                                    </Card>
+                                );
+                            })}
                         </Box>
                     </Grid>
-                    <Grid item xs={12} md={4} lg={3}> {/* DIREITA */}
-                        <Box sx={{backgroundColor: '#FFF2DE', border: '1px solid #DCD1C0', padding: '30px 20px'}}>
-                            <Typography variant="h4" color="textPrimary">Endomarketing</Typography>
-                            <Typography color="textPrimary" sx={{margin: '15px 0'}}>Endomarketing está relacionado às
-                            ações de treinamento ou qualificação
-                            dos colaboradores da empresa
-                            visando um melhor serviço para o
-                            cliente. Marketing interno, devido ao
-                            nome, é usualmente confundido com
-                            Endomarketing mesmo sendo
-                            conceitos diferentes.</Typography>
-                            <Button variant="outlined" color="dark">Dispensar</Button>
+                    <Grid item xs={12} md={4} lg={3}>
+                        {" "}
+                        {/* DIREITA */}
+                        <Box
+                            sx={{
+                                backgroundColor: "#FFF2DE",
+                                border: "1px solid #DCD1C0",
+                                padding: "30px 20px",
+                            }}
+                        >
+                            <Typography variant="h4" color="textPrimary">
+                                Endomarketing
+                            </Typography>
+                            <Typography
+                                color="textPrimary"
+                                sx={{ margin: "15px 0" }}
+                            >
+                                Endomarketing está relacionado às ações de
+                                treinamento ou qualificação dos colaboradores da
+                                empresa visando um melhor serviço para o
+                                cliente. Marketing interno, devido ao nome, é
+                                usualmente confundido com Endomarketing mesmo
+                                sendo conceitos diferentes.
+                            </Typography>
+                            <Button variant="outlined" color="dark">
+                                Dispensar
+                            </Button>
                         </Box>
                     </Grid>
                 </Grid>
             </Container>
         </Box>
-    )
+    );
 }
